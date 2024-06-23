3d2d3300df571b04cd2a524f16fc2186367eef66
Chao Zhang 张超 <zhangchaohci@gmail.com>
2024-06-23 18:19:25 +0800

    Fix bugs

--- components/homepage/ToolBar.tsx
+++ components/homepage/ToolBar.tsx
@@ -25,8 +25,7 @@ export const ToolBar = (props: ToolBarProps) => {
   return (
     <div
       className={cn(
-        props.classes +
-          " bg-gray-50 z-50 flex flex-row gap-1 items-center w-screen overflow-auto",
+        props.classes + " bg-gray-50 flex flex-row gap-1 items-center z-50",
       )}
     >
       <div className="min-w-[150px] font-medium text-2xl flex-center text-center uppercase m-1">
